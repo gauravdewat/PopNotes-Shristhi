da1b5842b1526c7547b9a3e9fb0ca1067b40d049
Gaurav <gauravdewat2@gmail.com>
2018-03-17 02:52:16 +0530

    completed?

--- app/src/main/java/com/example/mon00b/popnotes/CameraActivity.java
+++ app/src/main/java/com/example/mon00b/popnotes/CameraActivity.java
@@ -6,6 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.*;
@@ -21,22 +31,30 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Objects;
 
 public class CameraActivity extends AppCompatActivity implements CvCameraViewListener2 {
 
     private static final String TAG = CameraActivity.class.getCanonicalName();
 
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
     private CameraBridgeViewBase mOpenCvCameraView;
-    private CascadeClassifier cascadeClassifier;
+    private CascadeClassifier circleClassifier;
+    private CascadeClassifier triangleClassifier;
+    private CascadeClassifier heartClassifier;
     private int mAbsoluteTriSize = 0;
     private Paint paint, bg, txt;
 
     private MatOfRect triangles;
+    private MatOfRect circles;
+    private MatOfRect hearts;
     private Mat mRgba;
     private Mat mGray;
     private Bitmap bitmap;
 
-    String drawTxt = "ladkhfj;had;fklgh;wekafgnj;lkaskdfjgal;jfgklsfgnksmfhgkasjdmhfglksjmfgnksjhfgksd,fgnskjdfhmgsl,fgmnksdf,jgnskjfdngksdfjhglser;jg;lsketjg", lol = "";
+    String drawTxt = "Hello World:Hello World:Hello World", lol = "";
 
 
     List<MatOfPoint> contours;
@@ -62,22 +80,57 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
 
         try {
             // Copy the resource into a temp file so OpenCV can load it
-            InputStream is = getResources().openRawResource(R.raw.myhaar);
-            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-            File mCascadeFile = new File(cascadeDir, "myhaar.xml");
-            FileOutputStream os = new FileOutputStream(mCascadeFile);
+            InputStream ts = getResources().openRawResource(R.raw.trianglesclassifier);
+            File trianglesDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mtrianglesFile = new File(trianglesDir, "trianglesclassifier.xml");
+            FileOutputStream tos = new FileOutputStream(trianglesDir);
 
             byte[] buffer = new byte[4096];
             int bytesRead;
-            while ((bytesRead = is.read(buffer)) != -1) {
-                os.write(buffer, 0, bytesRead);
+            while ((bytesRead = ts.read(buffer)) != -1) {
+                tos.write(buffer, 0, bytesRead);
             }
-            is.close();
-            os.close();
+            ts.close();
+            tos.close();
 
             // Load the cascade classifier
-            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-            cascadeClassifier.load(mCascadeFile.getAbsolutePath());
+            triangleClassifier = new CascadeClassifier(trianglesDir.getAbsolutePath());
+            triangleClassifier.load(trianglesDir.getAbsolutePath());
+
+
+            InputStream hs = getResources().openRawResource(R.raw.heartclassifier);
+            File heartDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mheartsFile = new File(heartDir, "heartclassifier.xml");
+            FileOutputStream hos = new FileOutputStream(heartDir);
+
+            byte[] buffer1 = new byte[4096];
+            int bytesRead1;
+            while ((bytesRead1 = hs.read(buffer1)) != -1) {
+                hos.write(buffer1, 0, bytesRead1);
+            }
+            hs.close();
+            hos.close();
+
+            // Load the cascade classifier
+            heartClassifier = new CascadeClassifier(trianglesDir.getAbsolutePath());
+            heartClassifier.load(trianglesDir.getAbsolutePath());
+
+            InputStream cs = getResources().openRawResource(R.raw.circlesclassifier);
+            File circleDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mcircleFile = new File(circleDir, "circlesclassifier.xml");
+            FileOutputStream cos = new FileOutputStream(circleDir);
+
+            byte[] buffer2 = new byte[4096];
+            int bytesRead2;
+            while ((bytesRead2 = cs.read(buffer2)) != -1) {
+                cos.write(buffer2, 0, bytesRead2);
+            }
+            cs.close();
+            cos.close();
+
+            // Load the cascade classifier
+            circleClassifier = new CascadeClassifier(trianglesDir.getAbsolutePath());
+            circleClassifier.load(trianglesDir.getAbsolutePath());
 
         } catch (Exception e) {
             Log.d("CameraActivity", "Error loading cascade", e);
@@ -91,6 +144,37 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        /*databaseReference = FirebaseDatabase.getInstance().getReference().child("Notes");
+        Query query = databaseReference.orderByChild("userid").equalTo(mAuth.getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Model model = new Model();
+                    model = snapshot.getValue(Model.class);
+                    if (Objects.equals(model.getKey(), "triangle")) {
+
+                        final String triangle = model.getNotes();
+                    } else if (Objects.equals(model.getKey(), "circle")) {
+
+                        final String circle = model.getNotes();
+                    } else if (Objects.equals(model.getKey(), "rectangle")) {
+
+                        final String rectangle = model.getNotes();
+                    }
+
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -147,41 +231,57 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
         mGray = inputFrame.gray();
         Imgproc.cvtColor(mRgba, mGray, Imgproc.COLOR_RGBA2RGB);
         triangles = new MatOfRect();
-        android.graphics.Rect roi;
+        circles = new MatOfRect();
+        hearts = new MatOfRect();
+        android.graphics.Rect troi;
+        android.graphics.Rect croi;
+        android.graphics.Rect hroi;
 
-        if (cascadeClassifier.empty()) {
-            Log.d("ErrorInLoading", "Classifier not loaded");
-        }
-
-        if (cascadeClassifier != null) {
-            cascadeClassifier.detectMultiScale(mGray, triangles, 1.1, 2, 3,
+        if (triangleClassifier != null) {
+            triangleClassifier.detectMultiScale(mGray, triangles, 1.1, 2, 3,
                     new Size(mAbsoluteTriSize, mAbsoluteTriSize), new Size());
         }
 
+        if (heartClassifier != null) {
+            heartClassifier.detectMultiScale(mGray, hearts, 1.1, 2, 3,
+                    new Size(mAbsoluteTriSize, mAbsoluteTriSize), new Size());
+        }
+
+        if (circleClassifier != null) {
+            circleClassifier.detectMultiScale(mGray, circles, 1.1, 2, 3,
+                    new Size(mAbsoluteTriSize, mAbsoluteTriSize), new Size());
+        }
+
+
         Imgproc.cvtColor(mGray, mGray, Imgproc.COLOR_RGB2RGBA);
+
         Rect[] trianglesArray = triangles.toArray();
+        Rect[] circlesArray = circles.toArray();
+        Rect[] heartsArray = hearts.toArray();
+
         bitmap = Bitmap.createBitmap(mGray.cols(), mGray.rows(), Bitmap.Config.ARGB_8888, true);
         Utils.matToBitmap(mGray, bitmap);
 
         for (int i = 0; i < trianglesArray.length; i++) {
-            double xd1 = trianglesArray[i].tl().x;
-            double yd1 = trianglesArray[i].tl().y;
-            double xd2 = trianglesArray[i].br().x;
-            double yd2 = trianglesArray[i].br().y;
-            int ixd1 = (int) xd1;
-            int iyd1 = (int) yd1;
-            int ixd2 = (int) xd2;
-            int iyd2 = (int) yd2;
+            double txd1 = trianglesArray[i].tl().x;
+            double tyd1 = trianglesArray[i].tl().y;
+            double txd2 = trianglesArray[i].br().x;
+            double tyd2 = trianglesArray[i].br().y;
+            int tixd1 = (int) txd1;
+            int tiyd1 = (int) tyd1;
+            int tixd2 = (int) txd2;
+            int tiyd2 = (int) tyd2;
+            Log.d("triangles","Detected");
 
             // Create a rectangle around it
             //Imgproc.rectangle(mGray, trianglesArray[i].tl(), trianglesArray[i].br(), new Scalar(255, 0, 0, 255), 2);
-            roi = new android.graphics.Rect(ixd1, iyd1, ixd2 - ixd1, iyd2 - iyd1);
+            troi = new android.graphics.Rect(tixd1, tiyd1, tixd2 - tixd1, tiyd2 - tiyd1);
 
             Canvas canvas = new Canvas(bitmap);
-            canvas.drawRect(roi, paint);
-            canvas.drawRect(roi, bg);
-            int k = roi.right;
-            if (drawTxt.length() < roi.right)
+            canvas.drawRect(troi, paint);
+            canvas.drawRect(troi, bg);
+            int k = troi.right;
+     /*       if (drawTxt.length() < roi.right)
                 k = drawTxt.length();
             for (int j = 0; j < drawTxt.length(); ) {
                 lol += drawTxt.substring(j, k);
@@ -191,18 +291,110 @@ public class CameraActivity extends AppCompatActivity implements CvCameraViewLis
                     k = 2 * k;
                 else
                     k = drawTxt.length();
-            }
-Log.d("loltxt",lol);
+            }*/
+            Log.d("loltxt", lol);
             //canvas.drawText(lol, roi.right + (getResources().getDisplayMetrics().density * 2), roi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
-            float y = roi.bottom + (getResources().getDisplayMetrics().density * 10);
-            for (String line : lol.split(";")) {
-                canvas.drawText(line, roi.right + (getResources().getDisplayMetrics().density * 2), roi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
-                y += txt.descent() - txt.ascent();
+
+
+            int y = 0;
+            for (String line : drawTxt.split(":")) {
+                canvas.drawText(line, troi.right + (getResources().getDisplayMetrics().density * 2), troi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
+                y += (getResources().getDisplayMetrics().density * 8);
             }
 
             Log.d("Success", "Conversions Successful");
 
         }
+
+        for (int i = 0; i < circlesArray.length; i++) {
+            double cxd1 = circlesArray[i].tl().x;
+            double cyd1 = circlesArray[i].tl().y;
+            double cxd2 = circlesArray[i].br().x;
+            double cyd2 = circlesArray[i].br().y;
+            int cixd1 = (int) cxd1;
+            int ciyd1 = (int) cyd1;
+            int cixd2 = (int) cxd2;
+            int ciyd2 = (int) cyd2;
+
+            // Create a rectangle around it
+            //Imgproc.rectangle(mGray, trianglesArray[i].tl(), trianglesArray[i].br(), new Scalar(255, 0, 0, 255), 2);
+            croi = new android.graphics.Rect(cixd1, ciyd1, cixd2 - cixd1, ciyd2 - ciyd1);
+            Log.d("triangles","Detected");
+
+
+            Canvas canvas = new Canvas(bitmap);
+            canvas.drawRect(croi, paint);
+            canvas.drawRect(croi, bg);
+            int k = croi.right;
+     /*       if (drawTxt.length() < roi.right)
+                k = drawTxt.length();
+            for (int j = 0; j < drawTxt.length(); ) {
+                lol += drawTxt.substring(j, k);
+                lol += "\n";
+                j = k;
+                if (2 * k < drawTxt.length())
+                    k = 2 * k;
+                else
+                    k = drawTxt.length();
+            }*/
+            Log.d("loltxt", lol);
+            //canvas.drawText(lol, roi.right + (getResources().getDisplayMetrics().density * 2), roi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
+
+            int y = 0;
+            for (String line : drawTxt.split(":")) {
+                canvas.drawText(line, croi.right + (getResources().getDisplayMetrics().density * 2), croi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
+                y += (getResources().getDisplayMetrics().density * 8);
+            }
+
+            Log.d("Success", "Conversions Successful");
+
+        }
+
+        for (int i = 0; i < heartsArray.length; i++) {
+            double hxd1 = heartsArray[i].tl().x;
+            double hyd1 = heartsArray[i].tl().y;
+            double hxd2 = heartsArray[i].br().x;
+            double hyd2 = heartsArray[i].br().y;
+            int hixd1 = (int) hxd1;
+            int hiyd1 = (int) hyd1;
+            int hixd2 = (int) hxd2;
+            int hiyd2 = (int) hyd2;
+
+            // Create a rectangle around it
+            //Imgproc.rectangle(mGray, trianglesArray[i].tl(), trianglesArray[i].br(), new Scalar(255, 0, 0, 255), 2);
+            hroi = new android.graphics.Rect(hixd1, hiyd1, hixd2 - hixd1, hiyd2 - hiyd1);
+            Log.d("triangles","Detected");
+
+
+            Canvas canvas = new Canvas(bitmap);
+            canvas.drawRect(hroi, paint);
+            canvas.drawRect(hroi, bg);
+            int k = hroi.right;
+     /*       if (drawTxt.length() < roi.right)
+                k = drawTxt.length();
+            for (int j = 0; j < drawTxt.length(); ) {
+                lol += drawTxt.substring(j, k);
+                lol += "\n";
+                j = k;
+                if (2 * k < drawTxt.length())
+                    k = 2 * k;
+                else
+                    k = drawTxt.length();
+            }*/
+            Log.d("loltxt", lol);
+            //canvas.drawText(lol, roi.right + (getResources().getDisplayMetrics().density * 2), roi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
+
+
+            int y = 0;
+            for (String line : drawTxt.split(":")) {
+                canvas.drawText(line, hroi.right + (getResources().getDisplayMetrics().density * 2), hroi.bottom + (getResources().getDisplayMetrics().density * 10), txt);
+                y += (getResources().getDisplayMetrics().density * 8);
+            }
+
+            Log.d("Success", "Conversions Successful");
+
+        }
+
 
         Utils.bitmapToMat(bitmap, mGray);
         return mGray;
